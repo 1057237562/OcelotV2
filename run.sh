72ca0374e29f8794356627a1012ac1167357d194
fishBone000 <FishBoneEK@outlook.com>
2024-07-03 10:10:48 +0000

    Remove the cow

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-cowsay "777 is bad, don't do that ever again"
 systemctl stop ocelot
 g++ -fdiagnostics-color=always -g serverside.cpp -pthread -lssl -lcrypto -o serverside -std=c++17
 chmod 777 serverside
