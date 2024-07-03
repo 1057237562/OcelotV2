0f1c5fd1658625c4c5346877393278938d8026ed
Authenticator <1057237562@outlook.com>
2024-07-03 15:00:03 +0800

    update auth

--- serverside.cpp
+++ serverside.cpp
@@ -1,6 +1,7 @@
 #include "crypt"
 #include "libocelot"
 #include "unisocket"
+#include <fstream>
 #include <iostream>
 
 int main(void)
@@ -8,8 +9,19 @@ int main(void)
     using namespace unisocket;
     using namespace std;
     using namespace crypto;
+    ifstream config;
+    config.open("cfg");
+    int cnt;
+    config >> cnt;
+    vector<string> tokens(cnt);
+    for (int i = 0; i < cnt; i++) {
+        string username, password;
+        config >> username >> password;
+        tokens[i] = username + "\n" + password;
+    }
+
     TcpServer server = TcpServer("0.0.0.0", 3060);
-    ocelot::OcelotServer ocelot = ocelot::OcelotServer(server, { "libra\n65536forC", "Libra\n65536forC", "Stewie\njerky" });
+    ocelot::OcelotServer ocelot = ocelot::OcelotServer(server, tokens);
     cout << "Server Started!" << endl;
     ocelot.start();
     server.close();
