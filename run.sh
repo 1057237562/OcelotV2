011c77d037a260c62ecc5dce732ec66063b4c122
MinGW <MinGW@gnu.com>
2025-02-08 06:30:11 +0000

    mem leakd

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 systemctl stop ocelot
-g++ -fdiagnostics-color=always -g serverside.cpp -pthread -lssl -lcrypto -o serverside -std=c++17
+g++ -fdiagnostics-color=always -g serverside.cpp -pthread -lssl -lcrypto -o serverside -std=c++17 -fsanitize=address
 chmod 755 serverside
 systemctl start ocelot
 # -fsanitize=address
